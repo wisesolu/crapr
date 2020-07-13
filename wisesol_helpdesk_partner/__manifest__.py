3d517333e76b849335d6351a73d66352182fdc93
Zeno Nanon <zna@odoo.com>
2020-07-13 16:52:30 -0700

    [ADD] helpdesk module to auto-search partner

---
+++ wisesol_helpdesk_partner/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "WS: Setting Contact from Email Received to Helpdesk Ticket",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Task ID: 2279781
+    """,
+
+    'author': "Odoo Inc",
+    'website': "http://www.odoo.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'license': 'OEEL-1',
+    'depends': ['helpdesk'],
+    'data': [
+        'data/actions.xml'
+    ]
+}
