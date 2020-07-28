52a18ccc613c167d4269dd6b26971e9cdd2b7fcd
Zeno Nanon <zna@odoo.com>
2020-07-28 10:20:54 -0700

    [MOD] add multiple emails functionality

--- wisesol_helpdesk_partner/models/helpdesk_ticket.py
+++ wisesol_helpdesk_partner/models/helpdesk_ticket.py
@@ -8,11 +8,13 @@ class HelpdeskTicket(models.Model):
 
     def find_partner(self):
         for msg in self.message_ids:
-            if msg.author_id.name == self.env['ir.config_parameter'].sudo().get_param('voip_email'):
-                r = re.compile(r'\D\d{10}\D')
-                results = r.findall(msg.body)
-                if results:
-                    partners = self.env['res.partner'].search([('phone', 'like', '{}%{}%{}'.format(results[0][1:4],results[0][4:7],results[0][7:11]))])
-                    if partners:
-                        oldest = partners.sorted(key='x_studio_age', reverse=True)[0]
-                        self.partner_id = oldest
+            emails = self.env['ir.config_parameter'].sudo().get_param('voip_email').split()
+            for email in emails:
+                if msg.author_id.name == email:
+                    r = re.compile(r'\D\d{10}\D')
+                    results = r.findall(msg.body)
+                    if results:
+                        partners = self.env['res.partner'].search([('phone', 'like', '{}%{}%{}'.format(results[0][1:4],results[0][4:7],results[0][7:11]))])
+                        if partners:
+                            oldest = partners.sorted(key='x_studio_age', reverse=True)[0]
+                            self.partner_id = oldest
