8aea0167adf7b8cca7836210242a1ca06f1b018e
Zeno Nanon <zna@odoo.com>
2020-08-13 12:10:05 -0700

    [FIX] verify ticket was made through message

--- wisesol_helpdesk_partner/models/helpdesk_ticket.py
+++ wisesol_helpdesk_partner/models/helpdesk_ticket.py
@@ -9,18 +9,19 @@ class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     def find_partner(self):
-        msg = self.message_ids.sorted(key='create_date', reverse=True)[0]
-        emails = self.env['ir.config_parameter'].sudo().get_param('voip_email').split(",")
-        for email in emails:
-            if msg.author_id.email_normalized == email:
-                r = re.compile(r'\D\d{10}\D')
-                results = r.findall(msg.body)
-                if results:
-                    partners = self.env['res.partner'].search([('phone', 'like', '{}%{}%{}'.format(results[0][1:4],results[0][4:7],results[0][7:11]))])
-                    default_partner = self.env['res.partner'].search([('name', '=', 'alianza@opmits.com')])
-                    if partners:
-                        oldest = partners.sorted(key='x_studio_age', reverse=True)[0]
-                        self.partner_id = oldest
-                        self.partner_email = oldest.email
-                    else:
-                        self.partner_id = default_partner.id
+        if self.message_ids.sorted(key='create_date', reverse=True)[0] and self.env['ir.config_parameter'].sudo().get_param('voip_email').split(",")
+            msg = self.message_ids.sorted(key='create_date', reverse=True)[0]
+            emails = self.env['ir.config_parameter'].sudo().get_param('voip_email').split(",")
+            for email in emails:
+                if msg.author_id.email_normalized == email:
+                    r = re.compile(r'\D\d{10}\D')
+                    results = r.findall(msg.body)
+                    if results:
+                        partners = self.env['res.partner'].search([('phone', 'like', '{}%{}%{}'.format(results[0][1:4],results[0][4:7],results[0][7:11]))])
+                        default_partner = self.env['res.partner'].search([('name', '=', 'alianza@opmits.com')])
+                        if partners:
+                            oldest = partners.sorted(key='x_studio_age', reverse=True)[0]
+                            self.partner_id = oldest
+                            self.partner_email = oldest.email
+                        else:
+                            self.partner_id = default_partner.id
